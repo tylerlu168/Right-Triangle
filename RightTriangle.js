3bcb23f01baa201291bb6c9a6f6470830f9c55b8
tylerlu168 <tylerlu168@gmail.com>
2024-09-08 19:53:41 -0400

    Add files via upload

---
+++ RightTriangle.js
@@ -0,0 +1,11 @@
+function check()
+{
+    let a = document.getElementById("a").nodeValue;
+    let b = document.getElementById("b").nodeValue;
+    let c = document.getElementById("c").nodeValue;
+
+    let list = [a, b, c];
+    list.sort();
+
+
+}
